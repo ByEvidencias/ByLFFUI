35b3acaa49bd728b0468022d3c389e7dc99a6025
ByEvidencias <izanosky30@gmail.com>
2024-08-20 05:50:05 +0200

    Create Main.php

---
+++ src/LFF/ByEvidencias/Main.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LFF\ByEvidencias;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use LFF\ByEvidencias\command\LFFCommand;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use jojoe77777\SimpleForm;
+use jojoe77777\FormAPI;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+
+        
+        $this->getServer()->getCommandMap()->register("lff", new LFFCommand($this));
+    }
+
+    public function getLFFUI(Player $player): void {
+        $form = new SimpleForm(function (Player $player, ?int $data) {
+            if ($data === null) {
+                return;
+            }
+
+            $classes = [
+                0 => "Swordsman",
+                1 => "Archer",
+                2 => "Tank",
+                3 => "Miner",
+                4 => "Explorer",
+                5 => "Builder",
+                6 => "Farmer"
+            ];
+
+            if (isset($classes[$data])) {
+                $selectedClass = $classes[$data];
+                $config = $this->getConfig();
+                $lffMessage = $config->get("class-message", "{player} is looking for a faction and specializes in: {class}");
+
+                
+                $formMessage = str_replace(
+                    ["{player}", "{class}"],
+                    [$player->getName(), $selectedClass],
+                    $lffMessage
+                );
+
+                
+                $this->getServer()->broadcastMessage(TextFormat::colorize($formMessage));
+            }
+        });
+
+        $form->setTitle("§l§dLooking For Faction");
+        $form->setContent("§7Select your specialty");
+        $form->addButton(
+            "§eSwordsman",
+            0,
+            "textures/items/diamond_sword"
+        );
+        $form->addButton(
+            "§eArcher",
+            0,
+            "textures/items/bow_standby"
+        );
+        $form->addButton(
+            "§eTank",
+            0,
+            "textures/items/diamond_chestplate"
+        );
+        $form->addButton(
+            "§eMiner",
+            0,
+            "textures/ui/haste_effect"
+        );
+        $form->addButton(
+            "§eExplorer",
+            0,
+            "textures/items/spyglass"
+        );
+        $form->addButton(
+            "§eBuilder",
+            0,
+            "textures/blocks/brick"
+        );
+        $form->addButton(
+            "§eFarmer",
+            0,
+            "textures/items/diamond_hoe"
+        );
+        
+        $form->sendToPlayer($player);
+    }
+}
