bc8a41f19039a7b5b6bfe39848b09dc88006d208
ByEvidencias <izanosky30@gmail.com>
2024-08-24 04:57:20 +0200

    Update LFFCommand.php

--- src/LFF/ByEvidencias/command/LFFCommand.php
+++ src/LFF/ByEvidencias/command/LFFCommand.php
@@ -11,17 +11,15 @@ use LFF\ByEvidencias\Main;
 
 class LFFCommand extends Command {
 
-    private Main $plugin;
-
-    public function __construct(Main $plugin) {
+    public function __construct() {
         parent::__construct("lff", "Open the LFF menu", "/lff");
         $this->setPermission("lff.use");
-        $this->plugin = $plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
         if ($sender instanceof Player) {
-            $this->plugin->getLFFUI($sender);
+            $form = Main::getInstance()->getLFFUI();
+            $sender->sendForm($form);
             return true;
         } else {
             $sender->sendMessage("This command can only be executed by players.");
